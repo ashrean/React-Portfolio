e5e7db401715123c1819595df37f091771735624
Ashley Sese <sese.ashrean@gmail.com>
2023-03-06 22:47:02 -0800

    testing

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Link } from "react-router-dom"
-import NavBar from "../NavBar";
+import { Link } from "react-router-dom";
+import Portrait from "../../assets/images/Hawaii 2022.jpeg";
 
-function Header() {
-    return (
-        <header className='flex-row px-1'>
-            <div className='logo'>
-                <h1>
-                    <Link to="/"> Ashley Sese </Link>
-                </h1>
-            </div>
-            <Navbar></Navbar>
-        </header>
-    );
+function Home() {
+  return (
+    <section className="home my-5">
+      <div className="title">
+        <h2>Hi, I'm Ashley</h2>
+        <p>Welcome to my Student Portfolio!</p>
+        <Link to="portfolio">
+          <button>View my Projects</button>
+        </Link>
+      </div>
+      <div className="portrait">
+        <img
+          src={Portrait}
+          alt="self portrait"
+        />
+      </div>
+    </section>
+  );
 }
 
-export default Header;
+export default Home;
---
+++ src/components/Portfolio/portfolio.js
@@ -0,0 +1,97 @@
+import React from "react";
+
+import comingsoon2 from "../../../assets/images/comingsoon2.jpeg";
+
+function Portfolio() {
+  return (
+    <section class="portfolio">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 g-4">
+        <div class="col">
+          <div class="card">
+            <img src={Dashboard} alt="dashboard" class="card-img-top" />
+            <div class="card-body">
+              <a
+                href="https://github.com/captaiiinsolo/jobDashboardProject"
+                target="_blank"
+                rel="noreferrer">
+                <button>GitHub</button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <img src={NoteTaker} alt="noteTaker" class="card-img-top" />
+            <div class="card-body">
+              <a
+                href="https://github.com/ashrean/NoteTaker-hw11"
+                target="_blank"
+                rel="noreferrer">
+                <button>GitHub</button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <img src={GenerateReadme} alt="generateReadme" class="card-img-top" />
+            <div class="card-body">
+              <a
+                href="https://github.com/ashrean/generateREADME"
+                target="_blank"
+                rel="noreferrer">
+                <button>GitHub</button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <img src={comingsoon2} alt="Coming Soon" class="card-img-top" />
+            <div class="card-body">
+              <a
+                href="https://github.com/ashrean"
+                target="_blank"
+                rel="noreferrer">
+                <button>GitHub</button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <img src={comingsoon2} alt="Coming Soon" class="card-img-top" />
+            <div class="card-body">
+              <a
+                href="https://github.com/ashrean"
+                target="_blank"
+                rel="noreferrer">
+                <button>GitHub</button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <img src={comingsoon2} alt="Coming Soon" class="card-img-top" />
+            <div class="card-body">
+              <a
+                href="https://github.com/ashrean"
+                target="_blank"
+                rel="noreferrer">
+                <button>GitHub</button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </section>
+  );
+}
